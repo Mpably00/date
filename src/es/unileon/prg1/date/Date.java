889ad7b60ae58afcf6eedd29416cf631263b9d61
mpably00 <mpably00@estudiantes.unileon.es>
2018-03-27 22:29:14 +0200

    avances

--- src/es/unileon/prg1/date/Date.java
+++ src/es/unileon/prg1/date/Date.java
@@ -55,4 +55,48 @@ public class Date {
 		return this.day + "/" + this.month + "/" + this.year;
 	}
 
-}
+
+	String getmonthname(int month){
+		
+		switch(this.month){
+			case 1:
+				name="enero";
+				break;
+			case 2:
+				name="febrero";
+				break;
+			case 3:
+				name="marzo";
+				break;
+			case 4:
+				name="abril";
+				break;
+			case 5:
+				name="mayo";
+				break;
+			case 6:
+				name="junio";
+				break;
+			case 7:
+				name="julio";
+				break;
+			case 8:
+				name="agosto";
+				break;
+			case 9:
+				name="septiembre";
+				break;
+			case 10:
+				name="octubre";
+				break;
+			case 11:
+				name="noviembre";
+				break;
+			case 12:
+				name="diciembre";
+				break;
+		}
+		return name;
+		}}
+
+
